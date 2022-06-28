bf3835c124f75efa197bb8ae2ef776f5adabf411
marthamwangi <martyturing@gmail.com>
2022-06-28 06:33:18 +0300

    Changed browser to Chrome

--- karma.conf.js
+++ karma.conf.js
@@ -13,7 +13,7 @@ module.exports = function (config) {
       require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
-    browsers: ['ChromeHeadlessCI'],
+    browsers: ['Chrome'],
     customLaunchers: {
       ChromeHeadlessCI: {
         base: 'ChromeHeadless',
